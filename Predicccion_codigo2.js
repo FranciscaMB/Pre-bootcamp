255eb98ebf9d981dbc9102eee7daf98a0f987540
carlitatorres <carla.torres.programacion@gmail.com>
2022-08-19 10:57:37 -0400

    actualizando contenido

---
+++ Predicccion_codigo2.js
@@ -0,0 +1,13 @@
+function muestraInformaciónContacto() {
+    var auntContactInfo = ["Paula", "Smith", "Calle Principal 1234", "St. Louis", "MO", 12345];
+    console.log(auntContactInfo);
+}
+//la funcion m va a mostrar el arreglo completo                                                                                                                                             
+
+function muestraListaDeCompras() {
+    var produce = ["manzanas", "naranjas"];
+    var frozen = ["brócoli", "helado"];
+    frozen.push("croqueta de papa");
+    console.log(frozen);
+}
+//
